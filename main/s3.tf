a8e69dce38d440ca31721a618db7f1a05ffce2bf
Ravindra Singh <51900358+ravindrasinghh@users.noreply.github.com>
2020-08-28 20:08:42 +0530

    Create s3.tf

---
+++ main/s3.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-test-s3-terraform-bucket-git-build-4"
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+}

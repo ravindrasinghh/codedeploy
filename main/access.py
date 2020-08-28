1838b4c6f636591f2922fb514a8a9e50dc7a00c5
Varun Elavia <varunelavia@gmail.com>
2020-08-28 21:22:57 +0530

    Update access.py

--- main/access.py
+++ main/access.py
@@ -20,9 +20,8 @@ print(account_tokens['Credentials']['SecretAccessKey'])
 
 client = boto3.client(
         's3',
-        aws_access_key_id=ACCESS_KEY,
-        aws_secret_access_key=SECRET_KEY,
-        aws_session_token=SESSION_TOKEN,
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
     )
 print(client.list_buckets())
 

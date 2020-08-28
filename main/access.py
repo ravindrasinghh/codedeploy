5ad9c2d15f7cd4371efa8092e81928794f94fbab
Varun Elavia <varunelavia@gmail.com>
2020-08-28 21:31:19 +0530

    Update access.py

--- main/access.py
+++ main/access.py
@@ -11,28 +11,14 @@ role_arns = {
     "dev" : "arn:aws:iam::659604645969:role/Account-B-role"
 }
 
-print(role_arns[account])
-
 account_tokens = sts_connection.assume_role(
     RoleArn=role_arns[account],
-    RoleSessionName="session_of_account_1"
+    RoleSessionName="session_of_account"
 )
 
 os.environ["AWS_ACCESS_KEY_ID"] = account_tokens['Credentials']['AccessKeyId']
 os.environ["AWS_SECRET_ACCESS_KEY"] = account_tokens['Credentials']['SecretAccessKey']
 os.environ["AWS_SESSION_TOKEN"] = account_tokens['Credentials']['SessionToken']
-
-print(account_tokens['Credentials']['AccessKeyId'])
-print(account_tokens['Credentials']['SecretAccessKey'])
-print(account_tokens['Credentials']['SessionToken'])
-
-client = boto3.client(
-        's3',
-        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-        aws_session_token=os.environ["AWS_SESSION_TOKEN"],
-    )
-print(client.list_buckets())
 
 os.system("terraform init")
 

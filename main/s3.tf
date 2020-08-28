f7f162aaadb80a86d7e4fe6dcb8dfb322f4c4be1
Ravindra Singh <51900358+ravindrasinghh@users.noreply.github.com>
2020-08-28 21:31:09 +0530

    Update s3.tf

--- main/s3.tf
+++ main/s3.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-test-s3-terraform-bucket-git-build-4"
+  bucket = "my-test-s3-terraform-bucket-git-build-52"
   acl = "private"
   versioning {
     enabled = true
